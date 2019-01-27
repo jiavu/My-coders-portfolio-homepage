6c7778c1c71c15522cd1ce39093563b6a5a9ad1c
jiavu <39624614+jiavu@users.noreply.github.com>
2019-01-27 21:18:03 +0100

    Added a simple php script for email contact form.
    Not tested yet

--- plugins/email form/send_form_email.php
+++ plugins/email form/send_form_email.php
@@ -5,8 +5,8 @@
 	if(isset($_POST['email'])) {
 	
 		// EDIT THE 2 LINES BELOW AS REQUIRED
-		$email_to = "you@yourdomain.com";
-		$email_subject = "Your email subject line";
+		$email_to = "jan.p.tyra@gmail.com";
+		$email_subject = "Email from jiavu.de";
 	
 		function died($error) {
 			// your error code can go here
@@ -19,29 +19,35 @@
 	
 	
 		// validation expected data exists
-		if(!isset($_POST['first_name']) ||
-			!isset($_POST['last_name']) ||
-			!isset($_POST['email']) ||
-			!isset($_POST['telephone']) ||
-			!isset($_POST['comments'])) {
-			died('We are sorry, but there appears to be a problem with the form you submitted.');       
+		if(
+			/* !isset($_POST['first_name']) || */
+			/* !isset($_POST['last_name']) || */
+			!isset($_POST['emailAddress']) ||
+			!isset($_POST['emailSubject']) ||
+			!isset($_POST['emailMessage']) ||
+			/* !isset($_POST['telephone']) || */
+			/* !isset($_POST['comments'])) { */
+			died('I am sorry, but there appears to be a problem with the form you submitted.');       
 		}
 	
 		
 	
-		$first_name = $_POST['first_name']; // required
-		$last_name = $_POST['last_name']; // required
-		$email_from = $_POST['email']; // required
-		$telephone = $_POST['telephone']; // not required
-		$comments = $_POST['comments']; // required
+		/* $first_name = $_POST['first_name']; // required */
+		/* $last_name = $_POST['last_name']; // required */
+		$email_address = $_POST['emailAddress']; // required
+		$email_subject = $_POST['emailSubject']; // required
+		$email_message = $_POST['emailMessage']; // required
+		/* $telephone = $_POST['telephone']; // not required */
+		/* $comments = $_POST['comments']; // required */
 	
 		$error_message = "";
 		$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 	
-		if(!preg_match($email_exp,$email_from)) {
+		if(!preg_match($email_exp,$email_address)) {
 			$error_message .= 'The Email Address you entered does not appear to be valid.<br />';
 		}
 		
+		/* 
 		$string_exp = "/^[A-Za-z .'-]+$/";
 		
 		if(!preg_match($string_exp,$first_name)) {
@@ -55,12 +61,13 @@
 		if(strlen($comments) < 2) {
 			$error_message .= 'The Comments you entered do not appear to be valid.<br />';
 		}
+		 */
 		
 		if(strlen($error_message) > 0) {
 			died($error_message);
 		}
 		
-		$email_message = "Form details below.\n\n";
+		$email_final = "Form details below.\n\n";
 
 		
 		function clean_string($string) {
@@ -70,25 +77,26 @@
 	
 		
 	
-		$email_message .= "First Name: ".clean_string($first_name)."\n";
-		$email_message .= "Last Name: ".clean_string($last_name)."\n";
-		$email_message .= "Email: ".clean_string($email_from)."\n";
-		$email_message .= "Telephone: ".clean_string($telephone)."\n";
-		$email_message .= "Comments: ".clean_string($comments)."\n";
+		//$email_message .= "First Name: ".clean_string($first_name)."\n";
+		//$email_message .= "Last Name: ".clean_string($last_name)."\n";
+		$email_final .= "Email: ".clean_string($email_address)."\n";
+		$email_final .= "Subject: ".clean_string($email_subject)."\n";
+		//$email_message .= "Telephone: ".clean_string($telephone)."\n";
+		//$email_message .= "Comments: ".clean_string($comments)."\n";
 	
 		// create email headers
-		$headers = 'From: '.$email_from."\r\n".
-					'Reply-To: '.$email_from."\r\n" .
+		$headers = 'From: '.$email_address."\r\n".
+					'Reply-To: '.$email_address."\r\n" .
 					'X-Mailer: PHP/' . phpversion();
-		@mail($email_to, $email_subject, $email_message, $headers);  
+		@mail($email_to, $email_subject, $email_final, $headers);  
 
-		?>		<!-- Why are the php tags placed this way? -->
+		/* ?>		<!-- Why are the php tags placed this way? -->
 		
 		<!-- include your own success html here -->
 		
 		Thank you for contacting us. We will be in touch with you very soon.
 		
-		<?php
+		<?php */
 	
 	}
 ?>
